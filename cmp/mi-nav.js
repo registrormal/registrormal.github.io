a40fd110f7c1d877a1928b7f29a12f13bd838e7d
registrormal <118639905+registrormal@users.noreply.github.com>
2022-11-19 18:35:32 -0600

    Update mi-nav.js

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -47,7 +47,7 @@ class MiNav extends HTMLElement {
         html += /* html */
           `<li>
             <a href=
-"registro.html">Registros</a>
+"registros.html">Registros</a>
           </li>`;
       }
       this.ul.innerHTML += html;
